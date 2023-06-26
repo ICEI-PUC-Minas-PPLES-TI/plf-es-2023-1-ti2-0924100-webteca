130424c0956b5a49f9c1c862797ddef79f202092
gabriel6988 <83712515+gabriel6988@users.noreply.github.com>
2023-06-25 22:45:02 -0300

    Delete src/Back-end directory

--- src/Back-end/sair_Avaliador.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION['Nome_Avaliador']);
-  unset($_SESSION['Senha_Avaliador']);
-  header("Location: index.html");
-?>
--- src/Back-end/sair_Leitor.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION['Nome_Leitor']);
-  unset($_SESSION['Senha_Leitor']);
-  header("Location: index.html");
-?>
